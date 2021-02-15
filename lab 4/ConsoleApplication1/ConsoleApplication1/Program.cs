72b9c19ffa068ebeac2900d4af012ca3a90402fe
mim <syedamim190013@gmail.com>
2021-02-15 19:39:17 +0600

    lab 4

---
+++ lab 4/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //...............Program 1
+
+            int n;//used in every program
+            Console.WriteLine("\nProgram 1\n");
+            Console.Write("Enter Array Size: ");
+            n = Convert.ToInt16(Console.ReadLine());
+            int[] array1 = new int[n];
+            Console.WriteLine("Enter Array {0} Element: ", n);
+            for (int i = 0; i < n; i++)
+            {
+                array1[i] = Convert.ToInt16(Console.ReadLine());
+            }
+            Console.Write("Revers of the Array: ");
+            for (int i = array1.Length - 1; i >= 0; i--)
+            {
+                Console.Write(array1[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+            
---
+++ lab 4/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //...............Program 1
+
+            int n;//used in every program
+            Console.WriteLine("\nProgram 5\n");
+            Console.Write("Enter Array Size: ");
+            n = Convert.ToInt16(Console.ReadLine());
+            int[] array5 = new int[n];
+            Console.WriteLine("Enter Array {0} Element(not negative numburs): ", n);
+            for (int i = 0; i < n; i++)
+            {
+                array5[i] = Convert.ToInt16(Console.ReadLine());
+            }
+            for (int i = 0; i < array5.Length; i++)
+            {
+                int num = array5[i];
+                if (num != -1)
+                {
+                    for (int j = i + 1; j < array5.Length; j++)
+                    {
+                        if (num == array5[j])
+                        {
+                            array5[j] = -1;
+                        }
+                    }
+                }
+
+            }
+            Console.Write("Eunique Elements: ");
+            for (int i = 0; i < array5.Length; i++)
+            {
+                if (array5[i] != -1)
+                {
+                    Console.Write(array5[i] + " ");
+                }
+            }
+            Console.WriteLine();
+
+            //...............END
+            Console.WriteLine("\nEND\n");
+            Console.ReadLine();
+
+        }
+    }
+}
